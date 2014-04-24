680e0e8d16201c77dacd2b1f2980713d2b1fbac7
unknown <mitchell.fokken@my.ndsu.edu>
2014-04-23 20:53:52 -0500

    Garage should be done....

--- ParkingGarage/src/Garage.java
+++ ParkingGarage/src/Garage.java
@@ -50,27 +50,23 @@ public class Garage implements Serializable {
       return false;      
     }
     
-    
-    public boolean reservationcheck()
-    {
-        return true; //added return to temporarily remove error
-    }
     public boolean addReserved(ParkingSession newReservation)
     {
-        if(reservationcheck())
+        for(int a = 0; a<Reserved.size(); a++)
         {
-            Reserved.add(newReservation);
-            return true;
-        }
-        else
+        if(Reserved.get(a).getSpot()== newReservation.getSpot() && (Reserved.get(a).getStartingtime() < 
+  newReservation.getStartingtime())  && (Reserved.get(a).getFinishtime()> newReservation.getFinishtime() ))
         {
             return false;
+        }  
         }
+        Reserved.add(newReservation);
+        return true;
         }
-    public boolean removeReservation(ParkingSession res)
+    public boolean removeReservation(ParkingSession res,boolean before)
     {
         Calendar current = Calendar.getInstance();
-        if(res.getStartingtime()< (current.getTimeInMillis() *(1/3600000)+24))
+        if(res.getStartingtime()< ((current.getTimeInMillis() *(1/3600000))+24)&& before)
                 {
                    return false;
                 }
@@ -88,12 +84,18 @@ public class Garage implements Serializable {
         return false;
     }
     
-	public boolean makeAvailable(ParkingSession newSpot)
+	public boolean makeAvailable(String user)
 	{
-		return true; //to hide errror
+            for(ParkingSession a : NotAvailable)
+		if(a.getUser().equals(user))
+                {
+                    NotAvailable.set(a.getSpot()-1, null );
+                    return true;
+                }
+            return false;
 	}
     
-    public int[] getAvailableSpots()
+    public int[] getUnavailableSpots()
     {
 	    //NotAvailable
 	    //Reserved
@@ -104,9 +106,12 @@ public class Garage implements Serializable {
 		//    temp[i] = i;
 	    //}
 	    int counter1 = 0;
+            Calendar now = Calendar.getInstance();
 	    for(int i=0; i<Reserved.size(); i++)
 	    {
-		 if(Reserved.get(i) != null)
+		 if(Reserved.get(i) != null && ((Reserved.get(i).getStartingtime()*(1/60000))-15 < 
+                         now.getTimeInMillis()*(1/60000))
+                         && (Reserved.get(i).getFinishtime()> now.getTimeInMillis() ))
 		 {
 			 counter1++;
 		 }
--- ParkingGarage/src/ParkingSession.java
+++ ParkingGarage/src/ParkingSession.java
@@ -16,12 +16,14 @@ public class ParkingSession {
     private int spot;
     private long starttime;
     private long finishtime;
+    private String user;
     
-    public ParkingSession(int newSpot)
+    public ParkingSession(int newSpot, String newusername)
     {
         spot = newSpot;
         currenttime = Calendar.getInstance();
-        starttime =  currenttime.getTimeInMillis() * (1/3600000);
+        starttime =  currenttime.getTimeInMillis();
+        user = newusername;
     }
     /**
      * write start and finish time as values 1-24
@@ -29,17 +31,25 @@ public class ParkingSession {
      * @param newstarttime
      * @param newfinishtime 
      */
-    public ParkingSession(int newSpot,int newstarttime, int newfinishtime, int month, int day, int year)
+    public ParkingSession(int newSpot,int newstarttime, int newfinishtime, int month, int day, int year, String username)
     {
         spot = newSpot;
         Calendar temp = Calendar.getInstance();
         Calendar temp1 = Calendar.getInstance();
         temp.set(year, month, day, newstarttime, 0);
         temp1.set(year , month, day, newfinishtime , 0);
-        starttime = temp.getTimeInMillis() * (1/3600000);
-        finishtime = temp1.getTimeInMillis() * (1/3600000);
+        starttime = temp.getTimeInMillis();
+        finishtime = temp1.getTimeInMillis();
+        user = username;
     }
-    
+    public void setUser(String name)
+    {
+        user= name;
+    }
+    public String getUser()
+    {
+        return user;
+    }
     public void setSpot(int newspot)
     {
         spot = newspot;
@@ -64,10 +74,11 @@ public class ParkingSession {
     {
         return finishtime;
     }
-    public void checkout()
+    public int checkout()
     {
         currenttime = Calendar.getInstance();
-        finishtime = currenttime.getTimeInMillis() * (1/3600000);
+        finishtime = currenttime.getTimeInMillis();
+        return getHours();
     }
     /**
      * finds the difference in hours.
@@ -77,7 +88,7 @@ public class ParkingSession {
      */
     public int getHours()
     {
-        long total = finishtime - starttime;
+        long total = (finishtime - starttime) * (1/3600000);
         if(total == 0)
         {
             return 1;
@@ -86,6 +97,7 @@ public class ParkingSession {
             return (int) total;
         }
     }
+    @Override
     public String toString()
     {
         String result = "";
